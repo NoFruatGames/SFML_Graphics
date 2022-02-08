35e7e8c36d34bec57a09124394264f9b950242ac
Ivan Gunko <gamercombat57@gmail.com>
2022-02-08 17:20:42 +0200

    Window_Init

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,2 +1,17 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
+int main()
+{
+	RenderWindow window(VideoMode(800, 600), "SFML_Application");
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		window.display();
+	}
+	return 0;
+}
