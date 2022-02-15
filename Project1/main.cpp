a8dd9928a88034715c31864debd93e112d88fb30
Ivan Gunko <gamercombat57@gmail.com>
2022-02-15 18:26:40 +0200

    Draw class

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,13 +1,31 @@
 #include <SFML/Graphics.hpp>
-#include <iostream>
-#include <string>
 using namespace sf;
-int f[19][19]
-{};
-sf::Color black_color(0,0,0);
-sf::Color blue_color(0, 255, 255);
-const float pixel_size = 53;
-int main()
+class drawFigura
+{
+public:
+	drawFigura(float x, float y, float width, float height,float rotation, float border, Color color, Color border_color,RenderWindow& window)
+	{
+		RectangleShape rec(Vector2f(width,height));
+		rec.setPosition(Vector2f(x, y));
+		rec.setRotation(rotation);
+		rec.setOutlineThickness(border);
+		rec.setOutlineColor(border_color);
+		rec.setFillColor(color);
+		window.draw(rec);
+	}
+	drawFigura(RenderWindow& window,float x, float y, float radius, int type,float rotation, float border, Color color, Color border_color)
+	{
+		CircleShape cir(radius, type);
+		cir.setPosition(Vector2f(x, y));
+		cir.setOutlineThickness(border);
+		cir.setOutlineColor(border_color);
+		cir.setFillColor(color);
+		cir.setRotation(rotation);
+		window.draw(cir);
+	}
+
+};
+void main()
 {
 	RenderWindow window(VideoMode(1000, 1000), "SFML_Application");
 	while (window.isOpen())
@@ -19,41 +37,11 @@ int main()
 				window.close();
 		}
 		window.clear(Color(255,255,255));
-		Image code;
-		code.loadFromFile("Debug/Code.png");
-		for (int i = 0; i < 19; ++i)
-		{
-			for (int j = 0; j < 19; ++j)
-			{
-				int r = (int)code.getPixel(j * pixel_size, i * pixel_size).r;
-				int g = (int)code.getPixel(j * pixel_size, i * pixel_size).g;
-				int b = (int)code.getPixel(j * pixel_size, i * pixel_size).b;
-				int a = (int)code.getPixel(j * pixel_size, i * pixel_size).a;
-				if (r > 0 && r < 20)
-					r = 0;
-				if (g > 0 && g < 20)
-					g = 0;
-				if (b > 0 && b < 20)
-					b = 0;
-
-				if (r > 200 && r < 255)
-					r = 255;
-				if (g > 200 && g < 255)
-					g = 255;
-				if (b > 200 && b < 255)
-					b = 255;
-				if (Color(r,g,b) == black_color)
-				{
-					f[i][j] = 0;
-				}
-				else
-				{
-					f[i][j] = 1;
-				}
-			}
-		}
-
+		drawFigura(500, 500, 200, 100, 0, 5, Color(159, 141, 117), Color(110, 108, 87), window);
+		drawFigura(window, 500, 600, 20, 3, 0, 5, Color(86,69,53), Color(80, 62, 38));
+		drawFigura(window, 660, 600, 20, 3, 0, 5, Color(86, 69, 53), Color(80, 62, 38));
 		window.display();
 	}
-	return 0;
+	
+	 
 }
